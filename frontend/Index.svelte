f5432d2524d8a98a1726b608f5a269b2bec0323c
Andrew Ridyard <andyrids.uncapped698@passmail.net>
2025-07-24 22:32:33 +0100

    chore: initial commit

---
+++ frontend/Index.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import { Block, BlockLabel  } from "@gradio/atoms";
+	import Button from "@gradio/button";
+	import { Download as DownloadIcon } from "@gradio/icons";
+	import type { Gradio } from "@gradio/utils";
+  import { tick } from "svelte";
+
+	// props passed from the `gradio` Python backend
+	export let gradio: Gradio<{click: never;}>;
+	export let label: string = "Download CSV";
+	// `value` will receive the JSON string from `DataFrameDownloadButton.postprocess`
+	export let value: string | null = null;
+	export let visible: boolean = true;
+	export let elem_id: string = "";
+	export let elem_classes: string[] = [];
+
+
+	
+</script>
+
+<Block {visible} {elem_id} {elem_classes}>
+	
+	<Button
+		on:click={() => downloadCSVData(value)}
+		value={label}
+		gradio={gradio}
+		interactive={!!value}
+		variant="primary"
+	/>
+
+	<button
+		on:click={() => downloadCSVData(value)}
+		class="lg primary"
+	>
+		{label}
+	</button>
+</Block>
+
+<style>
+
+</style>
